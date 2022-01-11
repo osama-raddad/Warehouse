b1f1f843e5ac1e671772b8a7c403e6553fca8986
Osama-Raddad <oama.s.raddad@gmail.com>
2022-01-12 00:41:28 +0100

    fix a bug in instanceResolver.kt for SINGLETON instantiation

--- src/main/kotlin/org/raddad/main/dsl/api/dependency/instanceResolver.kt
+++ src/main/kotlin/org/raddad/main/dsl/api/dependency/instanceResolver.kt
@@ -5,20 +5,19 @@ import core.dependency.entity.CreationPattern.NEW
 import core.dependency.entity.CreationPattern.SINGLETON
 import core.dependency.entity.Factory
 import core.warehouse.entity.Warehouse
-import java.util.concurrent.locks.ReentrantLock
 
 @PublishedApi
-internal val lock = ReentrantLock()
+internal const val lock = "Lock"
 
 @PublishedApi
 internal inline fun <reified T> Factory.resolveInstance(warehouse: Warehouse): T {
     val value = when (creationPattern) {
         NEW -> constructor(warehouse)
         SINGLETON -> {
-            lock.lock()
-            if (instance == null) instance = constructor(warehouse)
-            instance
-            lock.unlock()
+            synchronized(lock){
+                if (instance == null) instance = constructor(warehouse)
+                instance
+            }
         }
         REUSABLE -> {
             if (instance == null) instance = constructor(warehouse)
